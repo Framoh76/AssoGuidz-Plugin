49f500c02a9da5a757cf7aaae6df1d502a005c92
Alexis <19507396+Alexis@users.noreply.github.com>
2021-06-07 12:25:12 +0200

    woocommerce add_to_cart

--- archive_produits.php
+++ archive_produits.php
@@ -10,7 +10,7 @@ $type = array(
 
 //isset($_GET['type_activite']) ? $_GET['type_activite'] : 'any'
 
-$produits = get_posts($type);
+$activites = get_posts($type);
 //var_dump($produits);
 //get_post_meta($post->ID, 'reserver', true) != null
 ?>
@@ -19,30 +19,40 @@ $produits = get_posts($type);
 	<div class="row">
 
 		<?php
-		foreach($produits as $produit) {
-			$termsTypeActivite = get_the_terms($produit->ID, 'type_activite'); 
+		foreach($activites as $activite) {
+			$termsTypeActivite = get_the_terms($activite->ID, 'type_activite'); 
 			if ( $termsTypeActivite != null) {
 				$type_activite = $termsTypeActivite[0]->name;
 			}
 
-			//echo "Id: " . $produit->ID . "<br />";
-			//echo "Titre : " . $produit->post_title . "<br />";
+			//echo "Id: " . $activite->ID . "<br />";
+			//echo "Titre : " . $activite->post_title . "<br />";
 			//echo "Type d'activité : " . $type_activite . "<br />";
 			//echo "Type d'activité demandé : " . $_GET['type_activite'] . "<br />";
 
-			if ($type_activite == $_GET['type_activite'] && $produit->post_name != "add") {
+			if ($type_activite == $_GET['type_activite'] && $activite->post_name != "add") {
 			?>
 
 				<div class="col-4">
-					<h1 style="text-align: center"><?php echo $produit->post_title ?></h1>
-					<p><?php echo $produit->post_content ?></p>
+					<h1 style="text-align: center"><?php echo $activite->post_title ?></h1>
+					<p><?php echo $activite->post_content ?></p>
 					<p></p>
 					<div class="row">
 						<div class="col-6">
-							<a href="<?php echo $produit->guid ?>">Détails</a>
+							<a href="<?php echo $activite->guid ?>">Détails</a>
 						</div>
 						<div class="col-6">
-							<button id="add_to_cart">Add to cart</button>
+							<?php 
+								$id_produit = get_post_meta($activite->ID, 'id_product', true);
+								$produit = get_post($id_produit);
+							?>
+							<a 
+								href="?add-to-cart=<?php echo $produit->ID ?>" 
+								data-quantity="1"
+								class="button product_type_simple add_to_cart_button ajax_add_to_cart"
+								data-product_id==<?php echo $produit->ID ?>	
+								rel="nofollow">Ajouter au panier
+							</a>
 						</div>
 					</div>
 				</div>
--- single-activite.php
+++ single-activite.php
@@ -10,7 +10,6 @@
  */
 
 function add_WOOCOMERCE_Product( $action, $productId, $title, $description, $price) {
-
 	if( $action == '1') {
 		$post_id = wp_insert_post( array(
 			'post_title' => $title,
@@ -175,6 +174,108 @@ if (isset($_POST['form_action'])) {
 		$idProduct = add_WOOCOMERCE_Product( $_POST['new'], $productId, $_POST['title'], $_POST['content'], $_POST['prix']);
 
 		update_post_meta( $id, 'id_product', $idProduct);
+
+		function add_menu_item($item_post_title, $main_menu_id, $menu_id, $typeActivite) {
+			// Si un menu existe on return l'id du menu
+			if (wp_get_nav_menu_items($menu_id)) {
+				foreach (wp_get_nav_menu_items($menu_id) as $item) {
+					if ($item->title === $item_post_title) {
+						$id = $item->ID;
+					}
+				}
+			}
+
+			// Si n'existe pas créer l'item et return l'id
+			$item_id = wp_update_nav_menu_item($menu_id, $id, array(
+				'menu-item-title' => $item_post_title,
+				'menu-item-object' => 'post',
+				'menu-item-parent-id' => $main_menu_id,
+				'menu-item-url' => get_site_url() . "/produits?type_activite=" . $typeActivite,
+				'menu-item-status' => 'publish'
+			));
+
+			return $item_id;
+		}
+
+		function add_post_item($main_menu_id, $posts, $menu_id, $typeActivite) {
+			$id = 0;
+			$add_item = true;
+
+			foreach ($posts as $myPost) {
+				// N'affiche pas l'activité add (Qui permet d'ajouter une activité)
+				if ($myPost->post_name != "add") {
+					// Récupère le type d'activité du post
+					$type = get_the_terms($myPost->ID, 'type_activite');
+					// Vérification du type de l'activité
+					if ( $type[0]->slug == $typeActivite) {
+						// Si un menu existe on return l'id du menu
+						if (wp_get_nav_menu_items($menu_id)) {
+							foreach (wp_get_nav_menu_items($menu_id) as $item) {
+								if ($item->title == $myPost->post_title) {
+									$id = $item->ID;
+								}
+							}
+						}
+
+						// Si il n'existe pas on le créer
+						if ($add_item) {
+							wp_update_nav_menu_item($menu_id, $id, array(
+								'menu-item-title' => ($myPost->post_title),
+								'menu-item-object' => 'post',
+								'menu-item-parent-id' => $main_menu_id,
+								'menu-item-url' => $myPost->guid,
+								'menu-item-status' => 'publish'
+							));
+						}
+					}
+				}
+			}
+		}
+
+		$type = array(
+			'numberposts' 	=> '-1',
+			'post_status'	=> 'any',
+			'post_type' 	=> 'activite',
+			'orderby' 		=> 'date',
+			'order' 		=> 'ASC',
+			'post_status'    => 'any'
+		);
+
+		$mainTypes = array('Conférences', 'Visite in situ');
+
+		$posts = get_posts($type);
+
+		// Liste des menues d'entête
+		$menu = get_term_by('name', 'menu', 'nav_menu');
+		$menu_id = $menu->term_id;
+
+		foreach ($mainTypes as $mainType) {
+
+			if ($mainType == 'Visite in situ')
+				$typeUrl = "visite_in_situ";
+			else
+				$typeUrl = "";
+
+			// Ajoute les menus d'entête dans le menu principal
+			$item_id = add_menu_item($mainType, $menu_id, $menu_id, $typeUrl);
+
+			// Ajoute les sous-menus
+			if ($mainType == 'Conférences') {
+
+				$id = add_menu_item('Conférences en salle', $item_id, $menu_id, 'conference');
+
+				add_post_item($id, $posts, $menu_id, 'conference');
+
+				$id = add_menu_item('Visio conférence', $item_id, $menu_id, 'visio_conference');
+
+				add_post_item($id, $posts, $menu_id, 'visio_conference');
+
+			} else {
+				
+				add_post_item($item_id, $posts, $menu_id, 'visite_in_situ');
+
+			}
+		}
 
 		header('Location: '. get_site_url() . '/archives-des-activites/');
 	}
